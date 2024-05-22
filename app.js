4a0d288a39e631348eec87d7ba0d463266544bb0
paramesh-75 <parameshwaranit22@gmail.com>
2024-05-22 21:58:38 +0530

    books details completeted

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app =express()
 const bodyparser= require('body-parser')
 const exhbs = require('express-handlebars');
 const dbo = require('./db');
+const ObjectID = dbo.ObjectId;
 
 
 app.engine('hbs',exhbs.engine({layoutsDir:'views/', defaultLayout:'main', extname:'hbs'}))
@@ -13,41 +14,53 @@ app.use(bodyparser.urlencoded({extended:true}));
 
 app.get('/',async (req, res)=>{
     let message='';
-    try{
+    let edit_id, edit_book;
+
     let database = await dbo.getDatabase()
     const collection = database.collection('books');
     const curser = collection.find({})
     const books =await curser.toArray();
 
+
+    if(req.query.edit_id){
+        edit_id = req.query.edit_id;
+        edit_book = await collection.findOne({_id: new ObjectID(edit_id) })
+    }
+
     switch (req.query.status) {
         case '1':
             message= 'Inserted Successfully!'
+            break;
+        case '2':
+            message= 'Updated Successfully!'
             break;
     
         default:
             break;
     }
     res.render(
-        'main',{message,books}
+        'main',{message, books, edit_id, edit_book}
     )
-}
-catch (error) {
-    console.error('Failed to retrieve books:', error);
-    res.status(500).send('Internal Server Error');
-}
 })
 
 app.post('/store_book',async (req,res)=>{
-    try {
+    
     let database = await dbo.getDatabase()
     const collection = database.collection('books')
     let book = {title: req.body.title, author: req.body.author}
     await collection.insertOne(book)
-    return res.redirect('/?status=1');}
-    catch (error) {
-        console.error('Failed to store book:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    return res.redirect('/?status=1');
+    
+})
+
+app.post('/update_book/:edit_id',async (req,res)=>{
+    
+    let database = await dbo.getDatabase()
+    const collection = database.collection('books')
+    let book = {title: req.body.title, author: req.body.author}
+    const edit_id = req.body.edit_id;
+    collection.updateOne({_id: new ObjectID(edit_id)})
+    return res.redirect('/?status=2');
 })
 app.listen(8000,()=>{
     console.log('the port is running 8000 port');
